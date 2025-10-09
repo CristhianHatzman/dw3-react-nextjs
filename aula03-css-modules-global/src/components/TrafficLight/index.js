865990fec4b3a90d663398bb967034d985425dc0
Cristhian Hatzman <cristhianhatzman@gmail.com>
2025-10-09 19:38:50 -0300

    feat: adiciona uso de state na aula 03 para modificar cores do semáforo

--- aula03-css-modules-global/src/components/TrafficLight/index.js
+++ aula03-css-modules-global/src/components/TrafficLight/index.js
@@ -1,14 +1,41 @@
-import styles from "@/components/TrafficLight/TrafficLight.module.css"
+import styles from "@/components/TrafficLight/TrafficLight.module.css";
+
+import { useState } from "react";
 
 const TrafficLight = () => {
+  const [color, setColor] = useState("gray");
   return (
     <>
       <div className={styles.container}>
-        <h3 style={{marginTop: "30px"}}>Semáforo da FATEC:</h3>
-        <br />
-        <div className={`${styles.light} ${styles.red}`}></div>
-        <div className={`${styles.light} ${styles.yellow}`}></div>
-        <div className={`${styles.light} ${styles.green}`}></div>
+        <h3 style={{ marginTop: "30px" }}>Semáfro da FATEC: </h3>
+
+        <div
+          className={`${styles.light} ${
+            color == "red" ? styles.red : styles.gray
+          }`}
+        >
+          <button className="button" onClick={() => setColor("red")}>
+            Pare!
+          </button>
+        </div>
+        <div
+          className={`${styles.light} ${
+            color == "yellow" ? styles.yellow : styles.gray
+          }`}
+        >
+          <button className="button" onClick={() => setColor("yellow")}>
+            Atenção!
+          </button>
+        </div>
+        <div
+          className={`${styles.light} ${
+            color == "green" ? styles.green : styles.gray
+          }`}
+        >
+          <button className="button" onClick={() => setColor("green")}>
+            Prossiga
+          </button>
+        </div>
       </div>
     </>
   );
